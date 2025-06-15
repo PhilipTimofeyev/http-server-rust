4d60e1a5893f36cd643acc68773d1de262e30acc
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-06-15 10:10:00 -0700

    Refactor and add user-agent

--- src/main.rs
+++ src/main.rs
@@ -27,18 +27,35 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let echo = request_line.split("echo/");
-    let content = echo.last().unwrap().split_whitespace().next();
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    let (status_line, content) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "")
-        } else if request_line.contains("/echo") {
-        println!("{}",content.unwrap());
-            ("HTTP/1.1 200 OK", content.unwrap())
-        } else {
-            ("HTTP/1.1 404 Not Found", "")
-        };
+    let start_line = &http_request[0];
+    let start_line: Vec<_> = start_line.split(" ").collect(); 
+    
+    let user_agent = http_request.iter().find(|el| el.contains("User-Agent"));
+    
+    let mut status_line = "HTTP/1.1 200 OK";
+    let content = match start_line[1] {
+        "/" =>  "",
+        "/user-agent" =>  {
+            let (_key, user_agent) = user_agent.unwrap().split_once(": ").unwrap();
+            user_agent
+        },
+        _ => {
+            if start_line[1].contains("/echo") {
+                let echo = start_line[1].split("/echo/").last().unwrap();
+                echo
+            } else {
+                status_line = "HTTP/1.1 404 Not Found";
+                ""
+            }
+
+        }
+    };
 
     let content_length = content.len();
     let response = format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{content}");
