ba398116abdad4729ddaee478499122fe3f0c4dd
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-06-17 14:04:27 -0700

    Refactor to request struct and impl

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,9 @@ fn main() {
 fn handle_connection(mut stream: TcpStream, dir: Arc<String>) {
     let mut buf_reader = BufReader::new(&stream);
     let request = buf_reader.fill_buf().unwrap();
+    let mut request = parse_request(request);
 
-    let (mut headers, body) = parse_request(request);
-
-    let start_line = headers.remove(0);
-    let start_line = parse_start_line(start_line);
-
-    let (content, status) = parse_endpoint(start_line, &headers[..], &dir, &body);
+    let (content, status) = request.parse_endpoint(&dir);
 
     let response = format!(
         "{}\r\nContent-Type: {}\r\nContent-Encoding: {}\r\nContent-Length: {}\r\n\r\n{}",
@@ -54,6 +50,106 @@ fn handle_connection(mut stream: TcpStream, dir: Arc<String>) {
     );
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+struct Request {
+    headers: Vec<String>,
+    body: String,
+}
+
+impl Request {
+    fn parse_start_line(&mut self) -> StartLine {
+        let start_line = self.headers.remove(0);
+        let mut start_line: Vec<&str> = start_line.split_whitespace().collect();
+
+        let (_html, endpoint, verb) = (
+            start_line.pop().unwrap().to_string(),
+            start_line.pop().unwrap().to_string(),
+            start_line.pop().unwrap(),
+        );
+
+        let verb = match verb {
+            "GET" => Verb::GET,
+            "POST" => Verb::POST,
+            &_ => todo!(),
+        };
+
+        StartLine { verb, endpoint }
+    }
+
+    fn parse_endpoint(&mut self, dir: &str) -> (Content, StatusCode) {
+        let start_line = self.parse_start_line();
+        let encoding = self.parse_encoding();
+        let (content, status) = match start_line.endpoint.as_str() {
+            "/" => (Content::new("", "", &encoding), StatusCode::_200),
+            "/user-agent" => {
+                let user_agent = self.headers.iter().find(|el| el.contains("User-Agent"));
+                let (_key, user_agent) = user_agent.unwrap().split_once(": ").unwrap();
+                let content = Content::new(user_agent, "text/plain", &encoding);
+                (content, StatusCode::_200)
+            }
+            endpoint if endpoint.contains("/echo") => {
+                let message = endpoint.split("/echo/").last().unwrap();
+                let content = Content::new(message, "text/plain", &encoding);
+                (content, StatusCode::_200)
+            }
+            endpoint if endpoint.contains("/files") => {
+                let filename = endpoint.split("/files/").last().unwrap();
+                let filepath = format!("{dir}{filename}");
+                let filepath = Path::new(&filepath);
+
+                match start_line.verb {
+                    Verb::GET => {
+                        let read_file = fs::read_to_string(filepath);
+                        let content_type = self.parse_content_type();
+                        if let Ok(read_file) = read_file {
+                            let content = Content::new(&read_file, &content_type, &encoding);
+                            (content, StatusCode::_200)
+                        } else {
+                            (Content::new("", "", &encoding), StatusCode::_404)
+                        }
+                    }
+                    Verb::POST => {
+                        let _ = fs::write(filepath, &self.body);
+                        (Content::new("", "", &encoding), StatusCode::_201)
+                    }
+                }
+            }
+            _ => {
+                return (Content::new("", "", &encoding), StatusCode::_404);
+            }
+        };
+        (content, status)
+    }
+
+    fn parse_content_type(&self) -> String {
+        if let Some(content_type) = self
+            .headers
+            .iter()
+            .find(|header| header.contains("Content-Type"))
+        {
+            let (_key, content_type) = content_type.split_once(": ").unwrap();
+            content_type.to_string()
+        } else {
+            "application/octet-stream".to_string()
+        }
+    }
+
+    fn parse_encoding(&self) -> String {
+        if let Some(encoding) = self
+            .headers
+            .iter()
+            .find(|header| header.contains("Accept-Encoding"))
+        {
+            let (_key, encoding) = encoding.split_once(": ").unwrap();
+            match encoding {
+                "gzip" => encoding.to_string(),
+                _ => "".to_string(),
+            }
+        } else {
+            "".to_string()
+        }
+    }
 }
 
 enum StatusCode {
@@ -87,108 +183,13 @@ fn parse_args(args: Vec<String>) -> String {
     }
 }
 
-fn parse_request(request: &[u8]) -> (Vec<String>, String) {
+fn parse_request(request: &[u8]) -> Request {
     let mut request: Vec<String> = request.lines().map(|result| result.unwrap()).collect();
 
     let body = request.split_off(request.len() - 1).pop().unwrap();
     let headers = request;
 
-    (headers, body)
-}
-
-fn parse_start_line(start_line: String) -> StartLine {
-    let mut start_line: Vec<&str> = start_line.split_whitespace().collect();
-
-    let (_html, endpoint, verb) = (
-        start_line.pop().unwrap().to_string(),
-        start_line.pop().unwrap().to_string(),
-        start_line.pop().unwrap(),
-    );
-
-    let verb = match verb {
-        "GET" => Verb::GET,
-        "POST" => Verb::POST,
-        &_ => todo!(),
-    };
-
-    StartLine { verb, endpoint }
-}
-
-fn parse_content_type(headers: &[String]) -> String {
-    if let Some(content_type) = headers
-        .iter()
-        .find(|header| header.contains("Content-Type"))
-    {
-        let (_key, content_type) = content_type.split_once(": ").unwrap();
-        content_type.to_string()
-    } else {
-        "application/octet-stream".to_string()
-    }
-}
-
-fn parse_encoding(headers: &[String]) -> String {
-    if let Some(encoding) = headers
-        .iter()
-        .find(|header| header.contains("Accept-Encoding"))
-    {
-        let (_key, encoding) = encoding.split_once(": ").unwrap();
-        match encoding {
-            "gzip" => encoding.to_string(),
-            _ => "".to_string()
-
-        }
-    } else {
-        "".to_string()
-    }
-}
-
-fn parse_endpoint(
-    start_line: StartLine,
-    headers: &[String],
-    dir: &str,
-    body: &str,
-) -> (Content, StatusCode) {
-    let encoding = parse_encoding(headers);
-    let (content, status) = match start_line.endpoint.as_str() {
-        "/" => (Content::new("", "", &encoding), StatusCode::_200),
-        "/user-agent" => {
-            let user_agent = headers.iter().find(|el| el.contains("User-Agent"));
-            let (_key, user_agent) = user_agent.unwrap().split_once(": ").unwrap();
-            let content = Content::new(user_agent, "text/plain", &encoding);
-            (content, StatusCode::_200)
-        }
-        endpoint if endpoint.contains("/echo") => {
-            let message = endpoint.split("/echo/").last().unwrap();
-            let content = Content::new(message, "text/plain", &encoding);
-            (content, StatusCode::_200)
-        }
-        endpoint if endpoint.contains("/files") => {
-            let filename = endpoint.split("/files/").last().unwrap();
-            let filepath = format!("{dir}{filename}");
-            let filepath = Path::new(&filepath);
-
-            match start_line.verb {
-                Verb::GET => {
-                    let read_file = fs::read_to_string(filepath);
-                    let content_type = parse_content_type(headers);
-                    if let Ok(read_file) = read_file {
-                        let content = Content::new(&read_file, &content_type, &encoding);
-                        (content, StatusCode::_200)
-                    } else {
-                        (Content::new("", "", &encoding), StatusCode::_404)
-                    }
-                }
-                Verb::POST => {
-                    let _ = fs::write(filepath, body);
-                    (Content::new("", "", &encoding), StatusCode::_201)
-                }
-            }
-        }
-        _ => {
-            return (Content::new("", "", &encoding), StatusCode::_404);
-        }
-    };
-    (content, status)
+    Request { headers, body }
 }
 
 struct StartLine {
@@ -200,7 +201,7 @@ struct Content {
     content: String,
     content_type: String,
     length: usize,
-    encoding: String
+    encoding: String,
 }
 
 impl Content {
@@ -218,4 +219,3 @@ impl Content {
         }
     }
 }
-
