2f86c48551e99e90f6b31695baa26fa46928bbf5
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-06-18 23:02:53 -0700

    Add gzip encoder and refactor encoder to be enum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use codecrafters_http_server::ThreadPool;
+use flate2::write::GzEncoder;
+use flate2::Compression;
 use serde::de::{self, Deserializer};
-use serde::{Deserialize};
+use serde::Deserialize;
 use std::{
     collections::HashMap,
-    env, fs, fmt,
+    env, fmt, fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     path::Path,
@@ -43,21 +45,35 @@ fn handle_connection(mut stream: TcpStream, dir: Arc<String>) {
 
     let mut request = parse_request(request);
     let status = request.parse_endpoint(&dir);
-    request.parse_encoding();
-    buf_reader.consume(length);
 
+    if let Some(encoder) = &request.headers.accept_encoding {
+        match encoder {
+            Encoder::Gzip => request.gzip_encoder(),
+        }
+    }
+
+    buf_reader.consume(length);
     let response = format!(
         "{}\r\nContent-Type: {}\r\nContent-Encoding: {}\r\nContent-Length: {}\r\n\r\n",
         status,
         request
             .headers
             .content_type
-            .unwrap_or_else(|| "Unknown".to_string()),
-        "",
-        request.headers.content_length.unwrap_or(0),
+            .unwrap_or_else(|| "text/plain".to_string()),
+        request
+            .headers
+            .accept_encoding
+            .map_or("".to_string(), |a| a.to_string()),
+        request.headers.content_length.unwrap_or_default(),
     );
+
     stream.write_all(response.as_bytes()).unwrap();
-    stream.write_all(&request.body).unwrap();
+    stream.write_all(&request.body).unwrap()
+}
+
+#[derive(Debug)]
+enum Encoder {
+    Gzip,
 }
 
 struct Request {
@@ -70,8 +86,10 @@ struct Headers {
     #[serde(deserialize_with = "string_to_startline")]
     #[serde(rename = "Start-Line")]
     start_line: StartLine,
+    #[serde(default)]
+    #[serde(deserialize_with = "encoder_type")]
     #[serde(rename = "Accept-Encoding")]
-    accept_encoding: Option<String>,
+    accept_encoding: Option<Encoder>,
     #[serde(rename = "User-Agent")]
     user_agent: Option<String>,
     #[serde(rename = "Content-Type")]
@@ -80,6 +98,28 @@ struct Headers {
     #[serde(deserialize_with = "string_to_option_usize")]
     #[serde(rename = "Content-Length")]
     content_length: Option<usize>,
+}
+
+fn encoder_type<'de, D>(deserializer: D) -> Result<Option<Encoder>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: Option<&str> = Option::deserialize(deserializer)?;
+    match s {
+        Some(encoder) if !encoder.is_empty() => match encoder {
+            encoder if encoder.contains("gzip") => Ok(Some(Encoder::Gzip)),
+            &_ => Ok(None),
+        },
+        _ => Ok(None),
+    }
+}
+
+impl fmt::Display for Encoder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Encoder::Gzip => write!(f, "gzip"),
+        }
+    }
 }
 
 fn string_to_option_usize<'de, D>(deserializer: D) -> Result<Option<usize>, D::Error>
@@ -101,20 +141,20 @@ where
 {
     let s: &str = Deserialize::deserialize(deserializer)?;
 
-            let mut start_line: Vec<&str> = s.split_whitespace().collect();
-            let (_html, endpoint, verb) = (
-                start_line.pop().unwrap().to_string(),
-                start_line.pop().unwrap().to_string(),
-                start_line.pop().unwrap(),
-            );
+    let mut start_line: Vec<&str> = s.split_whitespace().collect();
+    let (_html, endpoint, verb) = (
+        start_line.pop().unwrap().to_string(),
+        start_line.pop().unwrap().to_string(),
+        start_line.pop().unwrap(),
+    );
 
-            let verb = match verb {
-                "GET" => Verb::GET,
-                "POST" => Verb::POST,
-                &_ => todo!(),
-            };
+    let verb = match verb {
+        "GET" => Verb::GET,
+        "POST" => Verb::POST,
+        &_ => todo!(),
+    };
 
-            Ok(StartLine { verb, endpoint })
+    Ok(StartLine { verb, endpoint })
 }
 
 impl Request {
@@ -139,8 +179,17 @@ impl Request {
 
     fn handle_user_agent(&mut self) -> StatusCode {
         let user_agent = self.headers.user_agent.clone().unwrap_or_default();
+        self.headers.content_length = Some(user_agent.len());
         self.body = user_agent.as_bytes().to_vec();
         StatusCode::_200
+    }
+
+    fn gzip_encoder(&mut self) {
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+        encoder.write_all(&self.body).unwrap();
+        let compressed_body = encoder.finish().unwrap();
+        self.headers.content_length = Some(compressed_body.len());
+        self.body = compressed_body;
     }
 
     fn handle_echo(&mut self) -> StatusCode {
@@ -173,21 +222,6 @@ impl Request {
         let _ = fs::write(filepath, &self.body);
         self.headers.content_length = Some(self.body.len());
         StatusCode::_201
-    }
-
-    fn parse_encoding(&mut self) {
-        let encoding = match &self.headers.accept_encoding {
-            Some(encoding) => {
-                if encoding.to_string().contains("gzip") {
-                    Some("".to_string())
-                } else {
-                    Some("".to_string())
-                }
-            }
-            None => Some("".to_string()),
-        };
-
-        self.headers.accept_encoding = encoding;
     }
 }
 
